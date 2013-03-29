119d7cf346890062f85d87d7783ca2fa65bb79c6
David Bonnefoy <david.bonnefoy@information-addicts.com>
2013-03-29 10:13:49 +0100

    IA5 : Corrected description and hash for types using PBArrays.

--- src/compiler/objc_primitive_field.cc
+++ src/compiler/objc_primitive_field.cc
@@ -739,9 +739,9 @@ namespace google { namespace protobuf { namespace compiler { namespace objective
   void RepeatedPrimitiveFieldGenerator::GenerateDescriptionCodeSource(io::Printer* printer) const {
     if (ReturnsPrimitiveType(descriptor_)) {
 		printer->Print(variables_,
-			"listCount=self.$list_name$.count;\n"
-	        "for(int i=0;i<listCount;i++){\n"
-				"  [output appendFormat:@\"%@%@: %@\\n\", indent, @\"$name$\", [self.$list_name$ $array_value_type_name$AtIndex:i]];\n"
+			"NSUInteger $list_name$Count=self.$list_name$.count;\n"
+	        "for(int i=0;i<$list_name$Count;i++){\n"
+				"  [output appendFormat:@\"%@%@: %d\\n\", indent, @\"$name$\", [self.$list_name$ $array_value_type_name$AtIndex:i]];\n"
 	        "}\n");
 		//RAGY need to adjust the string format from %@
     } else {
@@ -762,8 +762,8 @@ namespace google { namespace protobuf { namespace compiler { namespace objective
   void RepeatedPrimitiveFieldGenerator::GenerateHashCodeSource(io::Printer* printer) const {
     if (ReturnsPrimitiveType(descriptor_)) {
       printer->Print(variables_,
-		"listCount=self.$list_name$.count;\n"
-        "for(int i=0;i<listCount;i++){\n"
+		"NSUInteger $list_name$Count=self.$list_name$.count;\n"
+        "for(int i=0;i<$list_name$Count;i++){\n"
 	        "hashCode = hashCode * 31 + [self.$list_name$ $array_value_type_name$AtIndex:i];\n"
         "}\n");
     } else {
