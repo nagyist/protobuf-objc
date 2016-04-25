2062c575c80977ae3f1acccbecc5e496a2664dc1
Peter Kolpashchikov <peter.kolpashchikov@corp.badoo.com>
2016-04-25 13:43:37 +0100

    version number

--- src/compiler/main.cc
+++ src/compiler/main.cc
@@ -23,7 +23,7 @@ using namespace google::protobuf::compiler::objectivec;
 int main(int argc, char **argv)
 {
 	if (argc == 2 && strcmp(argv[1], "-version") == 0) {
-		std::cout << "1.0.1" << std::endl;
+		std::cout << "1.0.2" << std::endl;
 		exit(0);
 	}
 
